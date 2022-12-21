cb3a87591285f0a2f3c3a4b39a0f90c2e52dfb67
Estuardo Villeda <estuardovilleda2001@gmail.com>
2022-12-21 10:02:49 -0600

    feat: monstrar titulo de la pagina en el breadcrumbs

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -14,14 +14,47 @@ const routes: Routes = [
     path: 'dashboard',
     component: PagesComponent,
     children: [
-      { path: '', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: 'promesas', component: PromesasComponent },
-      { path: 'rxjs', component: RxjsComponent },
+      {
+        path: '',
+        component: DashboardComponent,
+        data: {
+          titulo: 'Dashboard',
+        },
+      },
+      {
+        path: 'progress',
+        component: ProgressComponent,
+        data: {
+          titulo: 'ProgressBar',
+        },
+      },
+      {
+        path: 'grafica1',
+        component: Grafica1Component,
+        data: {
+          titulo: 'Gráfica #1',
+        },
+      },
+      {
+        path: 'promesas',
+        component: PromesasComponent,
+        data: {
+          titulo: 'Promesas',
+        },
+      },
+      {
+        path: 'rxjs',
+        component: RxjsComponent,
+        data: {
+          titulo: 'RxJs',
+        },
+      },
       {
         path: 'account-settings',
         component: AccountSettingsComponent,
+        data: {
+          titulo: 'Ajustes de cuenta',
+        },
       },
     ],
   },
--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import {
+  filter,
+  interval,
+  map,
+  Observable,
+  retry,
+  Subscription,
+  take,
+} from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
-  styleUrls: ['./rxjs.component.css']
+  styleUrls: ['./rxjs.component.css'],
 })
-export class RxjsComponent {
+export class RxjsComponent implements OnDestroy {
+  public internalSub: Subscription;
+  constructor() {
+    // this.retornaObservable()
+    //   .pipe(retry())
+    //   .subscribe(
+    //     (valor) => {
+    //       console.log('subs:', valor);
+    //     },
+    //     (error: any) => console.log(error),
+    //     () => console.log('obsterimnado')
+    //   );
 
+    this.internalSub = this.retornaIntervalo().subscribe(console.log);
+  }
+  ngOnDestroy(): void {
+    this.internalSub.unsubscribe();
+  }
+  retornaIntervalo(): Observable<number> {
+    return interval(100).pipe(
+      //take(10),
+      map((valor) => valor + 1),
+      filter((valor) => valor % 2 === 0)
+    );
+  }
+  retornaObservable() {
+    let i = -1;
+    return new Observable<number>((observer) => {
+      const intervalo = setInterval(() => {
+        i++;
+        observer.next(i);
+        if (i === 4) {
+          clearInterval(intervalo);
+          observer.complete();
+        }
+        if (i == 2) {
+          console.log('1 =2');
+          observer.error('i llego a dos');
+        }
+      }, 1000);
+    });
+  }
 }
