fc94b71147afe696a73a5d8d47b40d488b82d4bd
Estuardo Villeda <estuardovilleda2001@gmail.com>
2022-12-20 21:25:20 -0600

    chore: Navegacion sidebar y logout

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+
+declare function customInitFunction():void;
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -8,5 +10,9 @@ import { SettingsService } from '../services/settings.service';
 export class PagesComponent implements OnInit {
   constructor(private settingsService: SettingsService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    customInitFunction();
+  }
 }
+
+
