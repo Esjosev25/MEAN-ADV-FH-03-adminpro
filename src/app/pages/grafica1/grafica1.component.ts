4479515474a2539d6551fa295d987043297e0a26
Estuardo Villeda <estuardovilleda2001@gmail.com>
2022-12-20 18:47:40 -0600

    feat: graficas de donas

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-
+import { ChartData, ChartEvent, ChartType, Color } from 'chart.js';
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
-  styleUrls: ['./grafica1.component.css']
+  styleUrls: ['./grafica1.component.css'],
 })
-export class Grafica1Component {
-
-}
+export class Grafica1Component {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
+
+
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
 
